25ec5b519aae9974eed59eb8cf4fe08f9f155900
Andrew <Asaganda@gmail.com>
2015-07-30 23:02:00 -0400

    solved the 2nd day of challenges in methods-day-2.rb and added cash doubler dependency injection/message expectation example

--- methods-day-2.rb
+++ methods-day-2.rb
@@ -1,12 +1,24 @@
 def repeat(str, n)
   #takes a string and an integer and returns
-  # the string repeated n times, seperated by
+  # the string repeated n times, seperated by a space
+
+  # assigning an empty array to result
+  result = []
+  # takes a string and shovels itself into the result array
+  # and then duplicates itself n(we give it the number) times
+  n.times { result << str }
+  # then we join the strings together seperated by a space via .join enumerator
+  result.join(' ')
 end
 
 def interleave(array1, array2)
   #like interpose, but the array should not be nested
   #For example: given arrays [1, 2, 3] and [:a, :b, :c]
   #return [1, :a, 2, :b, 3, :c]
+
+  # interpose method does the method created in methods.rb file but we also need the array to be flat
+  # .flatten enumerator make the array 1 dimensional meaning its just one long array instead of nested
+  interpose(array1, array2).flatten
 end
 
 def hide_in_shuffle(letter)
@@ -14,6 +26,7 @@ def hide_in_shuffle(letter)
   #Your test should confirm that the returned array contains the original
   #letter
 
+  # what does this do?
   arr = [rand(10), rand(10), rand(10), letter]
   arr.shuffle
 end
@@ -25,5 +38,6 @@ def shuffler(array, n)
   #
   #Hint -- you aren't testing the return value of this method!
 
+  # the array is being shuffled n number of times
   n.times{ array.shuffle }
 end
